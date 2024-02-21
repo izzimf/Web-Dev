71447fc8b1c84816390816b78919892eeba14106
izzimf <124435153+izzimf@users.noreply.github.com>
2024-02-21 21:44:09 +0600

    Add files via upload

---
+++ lab4/src/app/products.ts
@@ -0,0 +1,131 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+  link: string;
+  image: string[];
+}
+
+export const products = [
+  {
+    id: 1,
+    name: 'Apple iPhone 13',
+    price: 291973,
+    description: '6.1-inch Super Retina XDR display',
+    link: 'https://kaspi.kz/shop/p/apple-iphone-13-128gb-midnight-chernyi-102298404/?c=750000000',
+    image: [
+      'https://resources.cdn-kaspi.kz/img/m/p/h32/h70/84378448199710.jpg?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/h35/h8f/84378448232478.jpg?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/h3d/h8e/64208874405918.jpg?format=gallery-medium'
+    ]
+  },
+  {
+    id: 2,
+    name: 'Apple iPhone 15 Pro Max',
+    price: 611884,
+    description: 'heavy-duty flagship made of titanium',
+    link: 'https://kaspi.kz/shop/p/apple-iphone-15-pro-max-256gb-seryi-113138420/?c=750000000',
+    image: [
+      'https://resources.cdn-kaspi.kz/img/m/p/hc1/h65/83559848181790.png?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/hfb/hf5/83559848214558.png?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/hcf/h00/83559848247326.png?format=gallery-medium'
+    ]
+  },
+  {
+    id: 3,
+    name: 'Apple iPhone 14',
+    price: 323135,
+    description: 'the iPhone 14\'s sensor it is more aperture f/1.5 rather than the 13\'s f/1.6',
+    link: 'https://kaspi.kz/shop/p/apple-iphone-14-128gb-chernyi-106363023/?c=750000000',
+    image: [
+      'https://resources.cdn-kaspi.kz/img/m/p/h98/h2b/64400497737758.jpg?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/hc8/h0b/64400500097054.jpg?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/hc2/hfe/64400502915102.jpg?format=gallery-medium'
+    ]
+  },
+  {
+    id: 4,
+    name: 'Apple iPhone 15',
+    price: 374801,
+    description: 'a smartphone that combines advanced optics, a powerful processor, a long-lasting battery and a memorable design',
+    link: 'https://kaspi.kz/shop/p/apple-iphone-15-128gb-chernyi-113137790/?c=750000000',
+    image: [
+      'https://resources.cdn-kaspi.kz/img/m/p/he2/h1d/83559338442782.png?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/h49/hd7/83559338475550.png?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/h23/hce/83559138459678.png?format=gallery-medium'
+    ]
+  },
+  {
+    id: 5,
+    name: 'Apple iPhone 13',
+    price: 289641,
+    description: 'Super Retina XDR, which features incredibly high pixel density - photos, videos and text look amazingly clear',
+    link: 'https://kaspi.kz/shop/p/apple-iphone-13-128gb-belyi-102298420/?c=750000000',
+    image: [
+      'https://resources.cdn-kaspi.kz/img/m/p/hc9/h90/64209083007006.jpg?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/h3b/h93/64209085235230.jpg?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/h8f/hce/64209121476638.jpg?format=gallery-medium'
+    ]
+  },
+  {
+    id: 6,
+    name: 'Apple iPhone 15 Pro Max',
+    price: 610337,
+    description: 'heavy-duty flagship made of titanium',
+    link: 'https://kaspi.kz/shop/p/apple-iphone-15-pro-max-256gb-sinii-113138428/?c=750000000',
+    image: [
+      'https://resources.cdn-kaspi.kz/img/m/p/h64/h50/83559848575006.png?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/hc5/hce/83559848640542.png?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/hab/hed/83559848706078.png?format=gallery-medium'
+    ]
+  },
+  {
+    id: 7,
+    name: 'Apple iPhone SE 2022',
+    price: 233804,
+    description: 'classic design with powerful hardware',
+    link: 'https://kaspi.kz/shop/p/apple-iphone-se-2022-64gb-chernyi-104153727/?c=750000000',
+    image: [
+      'https://resources.cdn-kaspi.kz/img/m/p/h92/haa/63993960136734.jpg?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/hc1/h7b/63993962364958.jpg?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/h13/h35/63993964527646.jpg?format=gallery-medium'
+    ]
+  },
+  {
+    id: 8,
+    name: 'Apple iPhone 11',
+    price: 223579,
+    description: 'Functional and stylish smartphone Apple iPhone 11 in a metal case can provide not only everyday communication and entertainment, but also productive work',
+    link: 'https://kaspi.kz/shop/p/apple-iphone-11-64gb-slim-box-chernyi-100692387/?c=750000000',
+    image: [
+      'https://resources.cdn-kaspi.kz/img/m/p/h3c/h70/63944536522782.jpg?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/h3c/h70/63944536522782.jpg?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/h3c/h70/63944536522782.jpg?format=gallery-medium'
+    ]
+  },
+  {
+    id: 9,
+    name: 'Apple iPhone 13 Pro Max',
+    price: 508735,
+    description: 'received a 6.7-inch Super Retina XDR1 display with an adaptive refresh rate of up to 120Hz and ProMotion technology for faster, smoother interactions',
+    link: 'https://kaspi.kz/shop/p/apple-iphone-13-pro-max-128gb-zolotistyi-102298917/?c=750000000',
+    image: [
+      'https://resources.cdn-kaspi.kz/img/m/p/he6/h2c/64109907345438.jpg?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/hbb/hdd/64109909508126.jpg?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/h81/h2d/64109911212062.jpg?format=gallery-medium'
+    ]
+  },
+  {
+    id: 10,
+    name: 'Apple iPhone 12',
+    price: 282540,
+    description: 'A14 Bionic, the iPhone\'s fastest processor',
+    link: 'https://kaspi.kz/shop/p/apple-iphone-12-128gb-fioletovyi-101395751/?c=750000000',
+    image: [
+      'https://resources.cdn-kaspi.kz/img/m/p/ha0/h22/64014163378206.jpg?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/ha0/h22/64014163378206.jpg?format=gallery-medium',
+      'ttps://resources.cdn-kaspi.kz/img/m/p/hcf/h00/83559848247326.png?format=gallery-medium'
+    ]
+  }
+];
