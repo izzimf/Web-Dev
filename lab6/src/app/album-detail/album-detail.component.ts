1f9675e25bb2f5270427698765b2a833a364466f
izzimf <124435153+izzimf@users.noreply.github.com>
2024-03-14 00:03:43 +0500

    Add files via upload

---
+++ lab6/src/app/album-detail/album-detail.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Album } from "../album";
+import {AlbumService} from "../albums.service";
+
+@Component({
+  selector: 'app-album-detail',
+  templateUrl: './album-detail.component.html',
+  styleUrl: './album-detail.component.css'
+})
+export class AlbumDetailComponent {
+  post: Album;
+  loaded: boolean;
+  newTitle: string;
+
+  constructor(private route: ActivatedRoute,
+              private albumService: AlbumService) {
+    this.post = {} as Album;
+    this.loaded = true;
+    this.newTitle = "";
+  }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((params) => {
+      let _id = params.get('id');
+      if (_id) {
+        let id = +_id;
+        this.loaded = false;
+        this.albumService.getPost(id).subscribe((post) => {
+          this.post = post;
+          this.loaded = true;
+        })
+      }
+    });
+  }
+
+  saveTitle() {
+    this.albumService.updateAlbum(this.post.id, this.newTitle).subscribe((response) => {
+      this.post.title = response.title;
+      this.newTitle = "";
+    })
+  }
+
+  back() {
+    window.location.href = "http://localhost:4200/albums";
+  }
+}
